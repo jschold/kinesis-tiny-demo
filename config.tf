f5343f8f25b8d7088a708778562794d2b07a82ab
jschold <jennaschold@gmail.com>
2020-07-11 16:01:15 -0700

    i am role policy addition

--- config.tf
+++ config.tf
@@ -8,11 +8,6 @@ resource "aws_kinesis_stream" "test_stream" {
   shard_count      = 1
   retention_period = 48
 
-  shard_level_metrics = [
-    "IncomingBytes",
-    "OutgoingBytes",
-  ]
-
   tags = {
     Environment = "test"
   }
@@ -20,9 +15,9 @@ resource "aws_kinesis_stream" "test_stream" {
 
 resource "aws_lambda_function" "process_lambda" {
   s3_bucket = "my-cigna-bucket"
-  s3_key    = "lambda.zip"
+  s3_key    = "jenna-pipeline/SourceArti/3XwrsGn.zip"
   function_name    = "process_lambda"
-  handler          = "process_data.lambda_handler"
+  handler          = "handlers/process_data.lambda_handler"
   runtime          = "python3.8"
   timeout          = 180
   role = "${aws_iam_role.lambda_exec_role.arn}"
@@ -30,12 +25,52 @@ resource "aws_lambda_function" "process_lambda" {
 
 resource "aws_lambda_function" "publish_lambda" {
   s3_bucket = "my-cigna-bucket"
-  s3_key    = "lambda.zip"
+  s3_key    = "jenna-pipeline/SourceArti/3XwrsGn.zip"
   function_name    = "publish_lambda"
-  handler          = "publish_data.lambda_handler"
+  handler          = "handlers/publish_data.lambda_handler"
   runtime          = "python3.8"
   timeout          = 180
   role = "${aws_iam_role.lambda_exec_role.arn}"
+}
+
+resource "aws_lambda_event_source_mapping" "cigna" {
+  event_source_arn  = "${aws_kinesis_stream.test_stream.arn}"
+  function_name     = "${aws_lambda_function.publish_lambda.arn}"
+  starting_position = "LATEST"
+}
+
+resource "aws_iam_role_policy" "kinesis_policy" {
+  name = "kinesis_policy"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Effect": "Allow",
+          "Action": [
+              "kinesis:GetRecords",
+              "kinesis:GetShardIterator",
+              "kinesis:DescribeStream",
+              "kinesis:ListStreams"
+          ],
+          "Resource": [
+              "arn:aws:kinesis:us-west-2:399394706053:stream/*"
+          ]
+      },
+      {
+          "Effect": "Allow",
+          "Action": [
+            "kinesis:PutRecord"
+          ],
+          "Resource": [
+            "arn:aws:kinesis:us-west-2:399394706053:stream/*"
+          ]
+        }
+  ]
+}
+  EOF
 }
 
 resource "aws_iam_role" "lambda_exec_role" {
@@ -56,3 +91,28 @@ resource "aws_iam_role" "lambda_exec_role" {
 }
 EOF
 }
+
+//resource "aws_iam_role_policy" "kinesis_policy" {
+//  name = "kinesis_policy"
+//  role = aws_iam_role.lambda_exec_role.id
+//
+//  policy = <<EOF
+//{
+//  "Version": "2012-10-17",
+//  "Statement": [
+//      {
+//          "Effect": "Allow",
+//          "Action": [
+//              "kinesis:GetRecords",
+//              "kinesis:GetShardIterator",
+//              "kinesis:DescribeStream",
+//              "kinesis:ListStreams"
+//          ],
+//          "Resource": [
+//              "arn:aws:kinesis:us-west-2:399394706053:stream/test_stream"
+//          ]
+//      }
+//  ]
+//}
+//  EOF
+//}
