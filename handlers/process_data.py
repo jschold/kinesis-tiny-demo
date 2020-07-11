65c26e8748557c32485da2c09124e5e8c0946a54
jschold <jennaschold@gmail.com>
2020-07-11 16:08:14 -0700

    add init

--- handlers/process_data.py
+++ handlers/process_data.py
@@ -2,6 +2,6 @@ import base64
 
 def lambda_handler(event, context):
     for record in event['Records']:
-       payload=base64.b64decode(record["kinesis"]["data"])
-       print(f"Message: {payload}")
-       
+        print(record)
+        payload=base64.b64decode(record["kinesis"]["data"])
+        print(f"Message: {payload}")
