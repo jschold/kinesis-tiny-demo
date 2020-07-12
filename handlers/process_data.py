822b4e8c78cc032242be45ee7efc7abd917162a9
jschold <jennaschold@gmail.com>
2020-07-12 11:09:56 -0700

    try this instead

--- handlers/process_data.py
+++ handlers/process_data.py
@@ -1,7 +1,8 @@
 import base64
 
+
 def lambda_handler(event, context):
     for record in event['Records']:
         print(record)
-        payload=base64.b64decode(record["kinesis"]["data"])
+        payload = base64.b64decode(record["kinesis"]["data"])
         print(f"Message: {payload}")
